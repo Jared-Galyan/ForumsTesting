9f6d2440f6258bbea064bdc7cb0d83d536536614
Jared Galyan <galyanja@gmail.com>
2020-12-27 20:41:46 -0500

    Yup

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, render_template, request, session
+import sqlite3
+import random
+
+app = Flask(__name__)
+app.secret_key = "Testing"
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        return render_template("index.html")
+    else:
+        if "email" in session:
+            return render_template("index.html")
+        else:
+            return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
